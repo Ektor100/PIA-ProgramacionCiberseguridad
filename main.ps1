a9eed084cd842bcc38812b6607cc04ef591da7d2
Ektor Kalev Hernandez <81501754+Ektor100@users.noreply.github.com>
2024-09-12 10:58:28 -0600

    add resource.psd1 and add main.ps1
    Se agrego el manifiesto de resourse y se empezo con la creacion del ps main, solo se conecto el modulo para la funcion 3 (Revision de recursos del sistema)

---
+++ main.ps1
@@ -0,0 +1,21 @@
+New-ModuleManifest -path "resourse.psd1" - Rootmodule "resourse.psm1"
+Import-Module "resourse.psm1"
+Get-Module Check-SystemResources
+Write-Host "Gracias por iniciar este programa el script se esta ejecutando... "
+do {
+    Write-Host "Escoja que funcion quiere iniciar: "
+    Write-Host "1) Revision de archivos por medio de Virus Total"
+    Write-Host "2) Listado de archivos ocultos"
+    Write-Host "3) Revision de uso de recursos"
+    Write-Host "4) [Tarea adicional]"
+
+    $option = Read-Host 
+
+    switch ($option) {
+        1 {Write-Host "Funcion no habilitada"}
+        2 {Write-Host "Funcion no habilitada"}
+        3 {Check-SystemResources}
+        4 {Write-Host "Funcion no habilitada"} 
+        default {Write-Host "Opcion invalida. Intentalo de nuevo."}
+    }
+} while ($option -ne 4)
