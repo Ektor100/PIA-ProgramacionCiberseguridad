ebe03b5110ab4eca8bf8475885d4481bf8a200f8
Ektor Kalev Hernandez <81501754+Ektor100@users.noreply.github.com>
2024-09-13 10:28:05 -0600

    add function-savePassword
    Co-Authored-By: Agustín S. <180478296+AgusS69@users.noreply.github.com>

--- main.ps1
+++ main.ps1
@@ -1,11 +1,13 @@
 New-ModuleManifest -path ".\resourse.psd1" -Rootmodule ".\resourse.psm1"
 Import-Module ".\resourse.psm1"
+New-ModuleManifest -path ".\savePassword.psd1" -Rootmodule ".\savePassword.psm1"
+Import-Module ".\savePassword.psm1"
 #New-ModuleManifest -path ".\Evidencia8_modulo.psd1" -RootModule "Evidencia8_modulo1.psm1" -Author "Grupo6" -Description "Este modulo va a usar verificacion de documentos locales creacion de un cvs para la facilitacion de lectura con la api de virus total, ademas de esto vamos a usar listado de documentos de dir y sus archivos escondidos" -ModuleVersion "1.0.0."
 #Import-Module ".\Evidencia8_modulo1.psm1" 
 #New-ModuleManifest -path ".\Evidencia8_modulo2.psd1" -RootModule "Evidencia8_modulo2.psm1" -Author "Grupo6" -Description "descrpcion del modulo" -ModuleVersion "1.0.0."
 #Import-Module ".\Evidencia8_modulo2.psm1"
-Get-Module Check-SystemResources
-Write-Host "---------0------------"
+Get-Module Check-SystemResources, Start-PasswordManagement
+Write-Host "===================================="
 Write-Host "Gracias por iniciar este programa el script se esta ejecutando... "
 
 do {
@@ -15,18 +17,18 @@ do {
     Write-Host "3) Revision de uso de recursos"
     Write-Host "4) [Tarea adicional]"
     Write-Host "5) Finalizar programa."
-    Write-Host "---------0------------"
+    Write-Host "===================================="
     $option = Read-Host 
-    Write-Host "---------0------------"
+    Write-Host "===================================="
     switch ($option) {
         1 {Write-Host "Funcion no habilitada"}
         2 {Write-Host "Funcion no habilitada"}
         3 {Check-SystemResources}
-        4 {Write-Host "Funcion no habilitada"} 
+        4 {Start-PasswordManagement} 
         5 {
             Write-Host "Finalizando..."
             Exit}
         default {Write-Host "Opcion invalida. Intentalo de nuevo."}
     }
-    Write-Host "---------0------------"
+    Write-Host "===================================="
 } while ($option -ne 5)
