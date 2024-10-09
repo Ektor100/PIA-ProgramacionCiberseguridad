3d4833125df9c2307a901a6156e31715d698da9d
Agustín S. <agustin.saldanaa@uanl.edu.mx>
2024-10-09 10:17:39 -0600

    Update main.sh

--- Entregable 2/main.sh
+++ Entregable 2/main.sh
@@ -5,5 +5,5 @@ source networkMonitoring.sh
 read -p "wich one option 1) Local dir Honeypot 2) Network Monitoring" $op
 case $op in 
     1) honeypot;;
-    2) checkAndInstallTools;;
+    2) mainNetworkMonitoring;;
 esac 
