66a057af08b14eea0c6f3619eca7c88768da13e2
Ektor Kalev Hernandez <81501754+Ektor100@users.noreply.github.com>
2024-10-09 13:02:43 -0600

    Actualizar main.sh

--- Entregable 2/main.sh
+++ Entregable 2/main.sh
@@ -2,7 +2,7 @@ source Honeypot.sh
 source networkMonitoring.sh
 
 # Main function menu
-read -p "wich one option 1) Local dir Honeypot 2) Network Monitoring" $op
+read -p "wich one option 1) Local dir Honeypot 2) Network Monitoring" op
 case $op in 
     1) honeypot;;
     2) mainNetworkMonitoring;;
